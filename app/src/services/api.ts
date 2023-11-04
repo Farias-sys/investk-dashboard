42d201850ae60ddc6ddb55720a7e8f0961be279c
Farias-sys <75812534+Farias-sys@users.noreply.github.com>
2023-11-04 17:16:16 -0300

    Support for production API connection

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url : string = (process.env.NODE_ENV === 'production') ? 'urlprod' : "http://localhost:8080/"
+const url : string = (process.env.NODE_ENV === 'production') ? 'https://investk-api.onrender.com/' : "http://localhost:8080/"
 
 const api = axios.create({
     baseURL: url
